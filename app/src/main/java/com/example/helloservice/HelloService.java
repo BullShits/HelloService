0db50d0a32a3bec05a5a34ea2841c6012cac0d76
ngói <nguyenngoi9190@gmail.com>
2015-10-27 13:26:32 +0700

    add play audio

--- app/src/main/java/com/example/helloservice/HelloService.java
+++ app/src/main/java/com/example/helloservice/HelloService.java
@@ -2,51 +2,73 @@ package com.example.helloservice;
 
 import android.app.Service;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.util.Log;
 
 /**
  * Created by admin on 西暦15/10/26.
  */
-public class HelloService extends Service {
+public class HelloService extends Service implements MediaPlayer.OnCompletionListener {
 
     int mStartMode;
     IBinder iBinder;
     boolean mAllowRebind;
-
+    private static final String TAG = "Service";
+    private MediaPlayer media;
 
 
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
 //        return null;
+        Log.e(TAG, "onBind");
+        Log.e(TAG, "----"+media.getDuration()+"-----");
         return iBinder;
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
+        media.stop();
+        media.release();
+        Log.e(TAG, "onDestroy");
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
 //        return super.onStartCommand(intent, flags, startId);
+        Log.e(TAG, "onStartCommand");
+        media.start();
+        Log.e(TAG, "----" + media.getDuration() / 1000 / 60 + "-----");
+        Log.e(TAG, "----" + media.getCurrentPosition() + "-----");
         return mStartMode;
     }
 
     @Override
     public void onCreate() {
         super.onCreate();
+        media = MediaPlayer.create(this, R.raw.about_you_now);
+        media.setOnCompletionListener(this);
+        Log.e(TAG, "onCreate");
     }
 
     @Override
     public boolean onUnbind(Intent intent) {
 //        return super.onUnbind(intent);
+        Log.e(TAG, "onUnbind");
         return mAllowRebind;
     }
 
     @Override
     public void onRebind(Intent intent) {
         super.onRebind(intent);
+        Log.e(TAG, "onRebind");
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+        Log.e(TAG, "MediaPlayer complete" + mp.getDuration());
     }
 }
--- app/src/main/java/com/example/helloservice/MainActivity.java
+++ app/src/main/java/com/example/helloservice/MainActivity.java
@@ -1,16 +1,41 @@
 package com.example.helloservice;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements SeekBar.OnSeekBarChangeListener {
+    private static final String TAG = "MainActivity";
+    Button btnPlay, btnStop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnPlay = (Button) findViewById(R.id.button);
+        btnStop = (Button) findViewById(R.id.button2);
+
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playMusic(v);
+            }
+        });
+
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopMusic(v);
+            }
+        });
     }
 
     @Override
@@ -33,5 +58,31 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void playMusic(View v){
+        Intent i = new Intent(this, HelloService.class);
+        startService(i);
+//        bindService
+    }
+
+    private void stopMusic(View v){
+        Intent i = new Intent(this, HelloService.class);
+        stopService(i);
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
     }
 }
